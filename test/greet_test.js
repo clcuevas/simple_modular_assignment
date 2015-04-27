d54dc6c5f5ede52676ec1ec201148ab8ad2c900f
Claudia Cuevas <omordn@gmail.com>
2015-04-27 16:12:38 -0700

    add basic greet module with test script

---
+++ test/greet_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var greet = require('../greet.js');
+
+describe('Test greet.js', function() {
+  it('should greet someone', function() {
+    expect(greet('world')).to.equal('hello world');
+  });
+});//end test description
