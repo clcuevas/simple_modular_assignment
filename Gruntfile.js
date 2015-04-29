b8ded08fb95508a83d4fad9b7391c82d7961f58a
Claudia Cuevas <omordn@gmail.com>
2015-04-28 17:34:51 -0700

    add mocah test in grunt, NOT WORKING

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = function(grunt) {
+  //loads the jshint package
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  //loads the mocha package
+  grunt.loadNpmTasks('mocha');
 
   grunt.initConfig({
     jshint: {
@@ -19,11 +22,21 @@ module.exports = function(grunt) {
           afterEach: true
         }
       }
+    },
+    mocha: {
+      dev: {
+        src: ['greet*.js', 'test/**/*/.js']
+      },
+      options: {
+        run: true
+      }
     }
   });
-  //this runs a jshint test in our dev sources
+  //this runs a jshint test task in our dev sources
   grunt.registerTask('test', ['jshint:dev']);
+  //this runs a mocha test task
+  grunt.registerTask('mocha', ['mocha:dev']);
   /*this allows you to define the 'grunt' command as a 
   command to execute the jshint:dev labeled test, which is listed in the array. For now, we are only using our 'test' task which we created on line 25*/
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('default', ['test', 'mocha']);
 };
